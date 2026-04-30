503354b1181fe4e68adce377f30d6280a2b0362d
williamnuez06-cripto <wiliamnuez06@gmail.com>
2026-04-30 12:14:28 +0200

    Refactorización: rename variable y extract method

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        saludar();
+        int resultado = 5 + 3;
+        System.out.println("Suma: " + resultado);
+    }
+
+    private static void saludar() {
         System.out.println("Hello World");
-        int suma = 5 + 3;
-        System.out.println("Suma: " + suma);
     }
 }
