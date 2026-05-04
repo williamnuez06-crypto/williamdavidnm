14f149a1ffec33994f8c064656c2364619f41658
williamnuez06-cripto <wiliamnuez06@gmail.com>
2026-05-04 12:24:34 +0200

    Codigo local modificado

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         saludar();
-        int resultado = 5 + 3;
+        int resultado = 11 + 3;
         System.out.println("Suma: " + resultado);
     }
 
