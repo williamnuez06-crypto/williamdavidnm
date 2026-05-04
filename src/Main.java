a250a85c0cf197f5eead5b9dd64004c2ff3fd56d
williamnuez06-cripto <wiliamnuez06@gmail.com>
2026-05-04 13:01:13 +0200

    Añado otro saludo en la nueva rama.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("bienvenido");
         saludar();
         int resultado = 11 + 3;
         System.out.println("Suma: " + resultado);
